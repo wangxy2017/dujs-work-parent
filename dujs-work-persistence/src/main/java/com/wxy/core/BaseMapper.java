ed35ab54d9c9df8828f54873a2bf26cd27c4c85f
wxy <243548880@qq.com>
2019-07-15 22:18:11 +0800

    demo

---
+++ dujs-work-persistence/src/main/java/com/wxy/core/BaseMapper.java
@@ -0,0 +1,60 @@
+package com.wxy.core;
+
+import java.util.List;
+
+public interface BaseMapper<T> {
+    /**
+     * 保存对象
+     *
+     * @param t
+     * @return
+     */
+    int save(T t);
+
+    /**
+     * 修改对象
+     *
+     * @param t
+     * @return
+     */
+    int update(T t);
+
+    /**
+     * 根据对象ID物理删除对象
+     *
+     * @param id
+     */
+    void delete(Integer id);
+
+    /**
+     * 根据对象ID逻辑删除对象
+     *
+     * @param id
+     */
+    void deleteActive(Integer id);
+
+    /**
+     * 条件查询对象列表
+     *
+     * @param t
+     * @return
+     */
+    List<T> queryList(T t);
+
+    /**
+     * 根据对象ID查询对象
+     *
+     * @param id
+     * @return
+     */
+    T queryById(Integer id);
+
+    /**
+     * 根据对象属性查询
+     *
+     * @param t
+     * @return
+     */
+    T queryByProperties(T t);
+}
+
---
+++ dujs-work-persistence/src/test/java/com/wxy/mapper/NoteMapperTest.java
@@ -0,0 +1,29 @@
+package com.wxy.mapper;
+
+import com.wxy.common.test.AutoValues;
+import com.wxy.entity.Note;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class NoteMapperTest {
+
+    @Autowired
+    private NoteMapper noteMapper;
+
+    @Test
+    public void TestSave() {
+        Note note = new Note();
+        note.setTitle(AutoValues.nextStr());
+        note.setContent(AutoValues.nextStr());
+        note.setPassword(AutoValues.nextStr());
+        int save = noteMapper.save(note);
+        log.info("保存笔记 save = {}", save > 0);
+    }
+}
