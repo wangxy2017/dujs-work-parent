978f109b160245c3e8bfcdd8610461cb8db3980b
wxy <wangxiaoyuan@pand-auto.com>
2019-07-19 13:42:19 +0800

    包扫描

--- dujs-work-notebook/src/main/java/com/wxy/note/Application.java
+++ dujs-work-notebook/src/main/java/com/wxy/note/Application.java
@@ -3,7 +3,7 @@ package com.wxy.note;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.wxy")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- dujs-work-persistence/src/main/java/com/wxy/Application.java
+++ dujs-work-persistence/src/main/java/com/wxy/Application.java
@@ -1,9 +1,11 @@
 package com.wxy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.wxy")
+@MapperScan(basePackages = "com.wxy.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
