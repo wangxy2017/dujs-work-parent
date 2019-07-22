618a135d98aeee8714d54ef9b03fa992d6506a25
wxy <wangxiaoyuan@pand-auto.com>
2019-07-22 14:55:46 +0800

    上传文件

--- dujs-work-bookmark/src/main/java/com/wxy/bookmark/controller/BookmarkController.java
+++ dujs-work-bookmark/src/main/java/com/wxy/bookmark/controller/BookmarkController.java
@@ -1,13 +1,21 @@
 package com.wxy.bookmark.controller;
 
 import com.wxy.common.response.ApiResponse;
+import com.wxy.common.tool.IDUtils;
 import com.wxy.entity.Bookmark;
 import com.wxy.service.BookmarkService;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,8 +32,25 @@ public class BookmarkController {
     private BookmarkService bookmarkService;
 
     @PutMapping
-    public void upload(@RequestParam("file") MultipartFile file) {
+    public ApiResponse upload(@RequestParam("file") MultipartFile file) throws IOException {
         log.info("上传文件：file = {}", file.getOriginalFilename());
+        if (file.getOriginalFilename().endsWith(".html")) {
+            File tempFile = new File("tmp-" + IDUtils.getUUID() + ".html");
+            file.transferTo(tempFile);
+            List<Bookmark> bookmarks = new ArrayList<>();
+            Document document = Jsoup.parse(tempFile,"UTF-8");
+            Elements elements = document.getElementsByTag("A");
+            elements.forEach(a -> {
+                Bookmark b = new Bookmark();
+                b.setIcon("123");
+                b.setHref(a.attr("HREF"));
+                b.setName(a.html());
+                bookmarks.add(b);
+            });
+            tempFile.delete();
+            return ApiResponse.success();
+        }
+        return ApiResponse.error();
     }
 
     @GetMapping("/list")
